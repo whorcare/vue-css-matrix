cc5fb877fc4c021d5d3b946df7f86cd51dcf8a76
zhangyufan <zhangyufan@moqipobing.com>
2020-10-31 15:51:14 +0800

    更灵活的input

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="change">变换</button>
+    
+    <div>scaleX:<input v-model="scaleX"/></div>
+    <div>scaleY:<input v-model="scaleY"/></div>
+    <div>translateX:<input v-model="translateX"/></div>
+    <div>translateY:<input v-model="translateY"/></div>
+    <div>rotateX:<input v-model="rotateX"/></div>
+    <div style="margin-top: 300px">
+      <img alt="Vue logo" class="img" :style="css" src="./assets/logo.png">
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { scale, translate, rotate, generateMatrix, generateCSS } from './matrix.js'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      css: null,
+      scaleX: 2,
+      scaleY: 2,
+      translateX: 50,
+      translateY: 50,
+      rotateX: 30
+    }
+  },
+  created() {
+  },
+  methods: {
+    change() {
+      const css = generateCSS(scale(this.scaleX, this.scaleY), translate(this.translateX, this.translateY), rotate('x', this.rotateX));  
+      console.log(css);
+      this.css = `transform: ${css}`;
+    }
   }
 }
 </script>
@@ -24,5 +49,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.img {
+  transition: all linear 2s;
+  width: 300px;
 }
 </style>
